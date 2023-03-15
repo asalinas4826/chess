b6cfdac13e6760b36e287f91ac00ded2d7d2fd00
Alejandro <asalinas4826@gmail.com>
2023-03-14 19:16:03 -0500

    piece selection done, turns done

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ import { PIECES, createBoard } from "./set_up.js"
 
 const board = createBoard()
 const boardElement = document.querySelector('.board')
+const turnTextElement = document.querySelector('.turn')
 const WHITE = true
 const BLACK = false
 let turn = WHITE
+let selectedTile    // last selected tile
 
 board.forEach(row => {
     row.forEach(tile => {
@@ -15,10 +17,57 @@ board.forEach(row => {
             tile.element.style.cursor = 'default'
         }
         tile.element.addEventListener('click', () => {
-            if (turn === tile.piece.white && tile.piece.name !== PIECES.EMPTY) {
-                console.log('sup')
+            console.log("white's turn? ", turn)
+            if (turn === tile.piece.white && tile.piece.name !== PIECES.EMPTY) { // click own piece, call selectPiece()
+                console.log('selected piece')
+                selectPiece(tile)
+            }
+            else if (selectedTile !== null && selectedTile !== undefined) { // anywhere else, attempt to move
+                console.log('tried to move')
+                if (checkValidMove(tile)) {
+                    movePiece(tile)
+                }
+                changeTurns()
             }
         })
         boardElement.append(tile.element)
     })
 })
+
+function changeTurns() {
+    turn = !turn
+    if (turn) {
+        turnTextElement.innerText = "White's move."
+    }
+    else {
+        turnTextElement.innerText = "Black's move."
+    }
+    board.forEach(row => {
+        row.forEach(tile => {
+            if (turn === tile.piece.white && tile.piece.name !== PIECES.EMPTY) {
+                tile.element.style.cursor = 'pointer'
+            }
+            else {
+                tile.element.style.cursor = 'default'
+            }
+        })
+    })
+    selectedTile = null
+}
+
+function selectPiece(tile) {
+    if (selectedTile !== null && selectedTile !== undefined) {
+        selectedTile.element.style.border = ''
+    }
+    selectedTile = tile
+    tile.element.style.border = '5px solid black'
+}
+
+function checkValidMove(tile) {
+    return true
+}
+
+function movePiece(tile) {
+    selectedTile.element.style.border = ''
+    selectedTile = null
+}
